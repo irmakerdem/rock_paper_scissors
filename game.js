43b5750934c31cfb1e213307a74991b629c324a0
Irmak Erdem <irmakerdem9@gmail.com>
2022-03-04 11:49:34 -0700

    Remove unnecessary properties from Player class

---
+++ game.js
@@ -0,0 +1,10 @@
+class Game {
+  constructor() {
+    this.gameType = ;
+  }
+}
+
+//when you click classic, assign this.gameType to "classic";
+
+// this.classicFighters = ["rock", "paper", "scissors"];
+// this.difficultFighters = ["rock", "paper", "scissors", "alien"];
