7f6c62f2329cba74d3a9d33e341d2e3ae4da9229
Irmak Erdem <irmakerdem9@gmail.com>
2022-03-04 09:34:58 -0700

    Set up initial player class structure

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //make player class
 //have a property of an array where array is ['rock', 'paper', 'scissors']
-//make both players be computers (so both can choose at random to see functionality)
 //make function that generates a random item from array
+//make both players be computers (so both can choose at random to see functionality)
 //input a return inside takeTurn() method
 
 
-//would be best to make a different class for each fighter
+//would be best to make a different class for each fighter??
 //OR have an array within an object within an array??
--- player.js
+++ player.js
@@ -1,8 +1,34 @@
 class Player {
   constructor() {
-
+    this.fighters = ["rock", "paper", "scissors"];
+    this.name = ;
+    this.token = ;
+    this.wins = 0;
   }
   takeTurn() {
-    
+
   }
 };
+
+
+//make function that generates a random item from array
+
+//returns random index value
+//math.floor returns  a whole number (rounds down) --> whole # is needed to access index in array
+//math.random returns a float between 0 and 1
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+//returns value in array
+function getRandomElement(array) {
+  return array[Math.floor(Math.random()*array.length)]
+}
+
+function randomizeFighter() {
+  getRandomElement(fighters)
+}
+
+function displayFighter() {
+
+}
