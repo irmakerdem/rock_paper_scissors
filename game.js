52abfe814a8fe937f844eb6d112c4ec5e119d941
Irmak Erdem <irmakerdem9@gmail.com>
2022-03-05 22:33:40 -0700

    Update Player class to have methods originally declared in Game class

--- game.js
+++ game.js
@@ -1,14 +1,10 @@
 class Game {
   constructor() {
     this.gameType = null;
-    this.classicFighters = ['rock', 'paper', 'scissors'];
-    this.user1 = new Player ('Irmak');
-    this.user2 = new Player ('computer');
-    this.user1Choice = null;
-    this.user2Choice = null;
+    this.user1 = new Player('human');
+    this.user2 = new Player('computer');
     this.isDraw = false;
-    this.winner = null;
-    // this.difficultFighters = ["rock", "paper", "scissors", "alien", "lizard"];
+    this.winner = this.determineClassicWinner();
   }
   determineGameType(gameId) {
     if (gameId === 'classicId') {
@@ -17,45 +13,55 @@ class Game {
       this.gameType = 'difficult'
     }
   }
-  determineUser1Choice(choiceId) {
-    if (choiceId === 'rockId'){
-      this.user1Choice = 'rock'
-    } else if (choiceId === 'paperId') {
-      this.user1Choice = 'paper'
-    } else if (choiceId === 'scissorsId') {
-      this.user1Choice = 'scissors'
-    }
-  }
-  determineUser2Choice() {
-    this.user2Choice = this.classicFighters[Math.floor(Math.random()*this.classicFighters.length)]
-  //   this.user1Choice = this.classicFighters[Math.floor(Math.random()*this.classicFighters.length)]
-  }
-  determineWinner() {
-    if (this.user1Choice === this.user2Choice) {
-      this.isDraw = true;
-      this.winner = null;
-    } else if (this.user1Choice === 'rock' && this.user2Choice === 'scissors') {
-      this.winner = 'human';
-      this.isDraw = false;
-    } else if (this.user1Choice === 'paper' && this.user2Choice === 'rock') {
-      this.winner = 'human';
+//chnage name to determineWinner not just classic
+  <p>🪨 > ✂️ & 🦎</p>
+} else if (this.user1.choice === 'rock' && (this.user2.choice === 'scissors' || this.user2.choice === 'lizard')) {
+  determineClassicWinner() {
+    if (this.user1.choice === this.user2.choice) {
+      this.isDraw = true
+      this.winner = null
+    } else if (this.user1.choice === 'rock' && this.user2.choice === 'scissors') {
+      this.user1.winRound()
       this.isDraw = false
-    } else if (this.user1Choice === 'scissors' && this.user2Choice === 'paper') {
-      this.winner = 'human';
-      this.isDraw = false;
+      return 'human'
+    } else if (this.user1.choice === 'paper' && this.user2.choice === 'rock') {
+      this.user1.winRound()
+      this.isDraw = false
+      return 'human'
+    } else if (this.user1.choice === 'scissors' && this.user2.choice === 'paper') {
+      this.user1.winRound()
+      this.isDraw = false
+      return 'human'
     } else {
-      this.winner = 'computer';
+      this.user2.winRound()
+      this.isDraw = false
+      return 'computer'
     }
-    this.updateScore();
-}
+    this.updateScore()
+  }
   updateScore() {
   if (this.winner === 'human') {
-    this.user1.takeTurn()
+
   } else if (this.winner === 'computer') {
-    this.user2.takeTurn()
+    this.user2.winRound()
   }
-}
+  }
 };
+
+
+
+// determineUser1Choice(choiceId) {
+//   if (choiceId === 'rockId'){
+//     this.user1Choice = 'rock'
+//   } else if (choiceId === 'paperId') {
+//     this.user1Choice = 'paper'
+//   } else if (choiceId === 'scissorsId') {
+//     this.user1Choice = 'scissors'
+//   }
+// }
+// determineUser2Choice() {
+//   this.user2Choice = this.classicFighters[Math.floor(Math.random()*this.classicFighters.length)]
+// }
 
 
 //keep score persistent when game type changes
--- player.js
+++ player.js
@@ -1,17 +1,50 @@
 class Player {
   constructor(playerName) {
     this.name = playerName;
-    // this.token = select from humanTokens array || '😃';
+    this.token = null;
     this.score = 0;
+    this.classicFighters = ['rock', 'paper', 'scissors'];
+    this.choice = this.takeTurnClassic();
+    this.choiceDif = this.takeTurnDifficult();
+    this.difficultFighters = ["rock", "paper", "scissors", "alien", "lizard"];
   }
-  // selectToken(){
-  //   this.humanTokens = ['🥳', '😎', '🤠']; //have this as default token in HTML: 😃
-  //   this.computerToken = 🤖 ///already is in HTML
-  // }
-  takeTurn() {
+  takeTurnClassic(choiceId) {
+    return choiceId || this.getRandomElement(this.classicFighters);
+  }
+  takeTurnDifficult(choiceId) {
+    return choiceId || this.getRandomElement(this.difficultFighters);
+  }
+  getRandomElement(array) {
+    return array[Math.floor(Math.random()*array.length)];
+  }
+  winRound() {
     this.score++;
-}
+  }
 };
+
+
+
+// if (playerName === 'human') {
+//   if (choiceId === 'rockId') {
+//     this.choice = 'rock'
+//   } else if (choiceId === 'paperId') {
+//     this.choice = 'paper'
+//   } else if (choiceId === 'scissorsId') {
+//     this.choice = 'scissors'
+//   }
+// } else if (playerName === 'computer') {
+//   this.choice = this.choice[Math.floor(Math.random()*this.classicFighters.length)]
+//   }
+
+
+
+    // this.token = select from humanTokens array || '😃';
+// selectToken(){
+//   this.humanTokens = ['🥳', '😎', '🤠']; //have this as default token in HTML: 😃
+//   this.computerToken = 🤖 ///already is in HTML
+// }
+
+
 
 // NOTES
 // math.floor returns  a whole number (rounds down) --> whole # is needed to access index in array
